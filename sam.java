3bfdd6c80621fec9a5319d59f201bb47d7475c83
Samyuktha2312 <52999349+Samyuktha2312@users.noreply.github.com>
2019-07-17 15:29:57 +0530

    Create sam.java

---
+++ sam.java
@@ -0,0 +1,13 @@
+public class sam { 
+public static void main(String[] args) {
+		int n=10;
+		if (n%2==0)
+		{
+			System.out.println("the number is even");
+		}
+		else
+		{
+			System.out.println("the number is odd");
+		}
+       
+	}
