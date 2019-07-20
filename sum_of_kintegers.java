bb6a438ef97d1083fe94c9f63517dca12b4315e3
Samyuktha2312 <52999349+Samyuktha2312@users.noreply.github.com>
2019-07-20 14:03:01 +0530

    Create sum_of_kintegers.java

---
+++ sum_of_kintegers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class sum_of_kintegers {
+
+	public static void main(String[] args) {
+	  Scanner s=new Scanner(System.in);
+	  System.out.println("Enter the value:");
+	  int n=s.nextInt();
+	  int k=s.nextInt();
+	  int sum=0;
+	  int a[]=new int[n];
+	  for(int i=0;i<n;i++)
+	  {
+		  a[i]=s.nextInt();
+	  }
+	  for(int i=0;i<n;i++)
+	  {
+		  for(int j=0;j<k;j++)
+		  {
+			  if(i==j)
+			  {
+				  sum=sum+a[i];
+			  }
+			}
+		   }
+	  System.out.println(sum);
+	}
+}
