6cb3bddfc51e6751e7accb8d7505290305d6fda5
Samyuktha2312 <52999349+Samyuktha2312@users.noreply.github.com>
2019-07-20 12:02:03 +0530

    Create print_ntimes.java

---
+++ print_ntimes.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class print_ntimes {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the n value");
+        int n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+        	String s1="Hello";
+        	System.out.println(s1);
+          }
+  
+	}
+
+}
