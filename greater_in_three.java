dca3dd7cbfe2049cfaf31f7335505ffad5f5a71d
Samyuktha2312 <52999349+Samyuktha2312@users.noreply.github.com>
2019-07-20 10:17:32 +0530

    Create greater_in_three.java

---
+++ greater_in_three.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class greater_in_three {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter three numbers");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		if(a>b&&a>c)
+			System.out.println(a);
+		else if(b>c&&b>a)
+			System.out.println(b);
+		else
+		     System.out.println(c);
+		s.close();
+
+	}
+
+}
