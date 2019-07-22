0ecf8f7bf458d7c56c0d99a7eeb052af8b7c32cc
Samyuktha2312 <52999349+Samyuktha2312@users.noreply.github.com>
2019-07-22 16:06:30 +0530

    Add files via upload

---
+++ find_maximum.java
@@ -0,0 +1,17 @@
+package rough;
+import java.util.*;
+public class find_maximum {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int a[] = new int[n];
+		for(int i = 0;i<n;i++)
+		{
+			a[i] = s.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.println(a[n-1]);
+	}
+
+}
